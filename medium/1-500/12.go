b7dfd4b57985efe60195d4405c351f4e6c56fa3c
Shizukiyo <0609aleksey@gmail.com>
2026-04-21 14:25:04 +0300

    50

--- medium/1-500/12.go
+++ medium/1-500/12.go
@@ -1,16 +1,14 @@
 // https://leetcode.com/problems/integer-to-roman/
 package main
 
-import "fmt"
-
 func intToRoman(num int) string {
-    values := make([]int, 0)
+	values := make([]int, 0)
 	add := 1
 	for {
-		values = append(values, (num % 10) * add)
+		values = append(values, (num%10)*add)
 		num /= 10
 		add *= 10
-		if num / 10 == 0 && num % 10 == 0 {
+		if num/10 == 0 && num%10 == 0 {
 			break
 		}
 	}
@@ -25,10 +23,14 @@ func intToRoman(num int) string {
 				res = "IX" + res
 			case value == 4:
 				res = "IV" + res
-			case value < 4: 
-					for range (value) {res = "I" + res}
-			case value < 9: 
-				for range (value-5) {res = "I" + res}
+			case value < 4:
+				for range value {
+					res = "I" + res
+				}
+			case value < 9:
+				for range value - 5 {
+					res = "I" + res
+				}
 				res = "V" + res
 			}
 
@@ -38,12 +40,12 @@ func intToRoman(num int) string {
 				res = "XC" + res
 			case value == 40:
 				res = "XL" + res
-			case value < 40: 
-				for i := 0; i < value; i+=10{
+			case value < 40:
+				for i := 0; i < value; i += 10 {
 					res = "X" + res
 				}
-			case value < 90: 
-				for i := 0; i < value-50; i+=10{
+			case value < 90:
+				for i := 0; i < value-50; i += 10 {
 					res = "X" + res
 				}
 				res = "L" + res
@@ -55,29 +57,22 @@ func intToRoman(num int) string {
 				res = "CM" + res
 			case value == 400:
 				res = "CD" + res
-			case value < 400: 
-				for i := 0; i < value; i+=100{
+			case value < 400:
+				for i := 0; i < value; i += 100 {
 					res = "C" + res
 				}
-			case value < 900: 
-				for i := 0; i < value-500; i+=100{
+			case value < 900:
+				for i := 0; i < value-500; i += 100 {
 					res = "C" + res
 				}
 				res = "D" + res
 			}
 		case value >= 1000:
-			for i := 0; i < value; i+= 1000 {
+			for i := 0; i < value; i += 1000 {
 				res = "M" + res
 			}
 		}
 	}
 
 	return res
-}
-
-func main() {
-	fmt.Println(intToRoman(3))
-	fmt.Println(intToRoman(3749))
-	fmt.Println(intToRoman(58))
-	fmt.Println(intToRoman(1994))
 }
---
+++ medium/1-500/50.go
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/powx-n/description/
+package main
+
+import (
+	"math"
+)
+
+func myPow(x float64, n int) float64 {
+	return math.Pow(x, float64(n))
+}
