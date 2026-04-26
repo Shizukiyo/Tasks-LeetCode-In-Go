7d189e372b32df8910e426d1e9b01f9b9a0c3c7f
Shizukiyo <0609aleksey@gmail.com>
2026-04-26 13:05:57 +0300

    118

---
+++ easy/1-500/118.go
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/pascals-triangle/description/
+package main
+
+func generate(numRows int) [][]int {
+	triangle := make([][]int, numRows)
+
+	for i := range numRows {
+		triangle[i] = make([]int, i+1)
+		triangle[i][0] = 1
+		triangle[i][i] = 1
+
+		for j := 1; j < i; j++ {
+			triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+		}
+	}
+
+	return triangle
+}
